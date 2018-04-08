a8aa17aa7275e46e4453437f0576ba8520cdb5ce
garg.ashish0@gmail.com <ashish.garg@appirio.com>
2018-04-08 23:15:18 +0530

    add test cases

--- test/test.js
+++ test/test.js
@@ -1,3 +1,23 @@
 /**
  * Created by Ashish on 4/8/18.
  */
+var supertest = require('supertest');
+var should = require('should');
+
+var server = supertest.agent("http://localhost:8080");
+
+describe("test for endpoint to get forecast data",function(){
+
+  it("should return data",function(done){
+
+    server
+      .get("/forecast/london")
+      .expect("Content-type",/json/)
+      .expect(200)
+      .end(function(err,res){
+        res.status.should.equal(200);
+        done();
+      });
+  });
+
+});
