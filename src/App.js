bc19c6ac236194dfbb4fb8b4286d8990a6265424
ashish-garg007 <ashish.garg@appirio.com>
2018-04-08 12:49:40 +0530

    change in package file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   componentDidMount() {
     let port = process.env.PORT || 8080;
-    let api_host = 'http://localhost:'+port;
+    let api_host = 'https://localhost:'+port;
     console.log(api_host);
     this.callApi(api_host)
       .then(res => this.setState({ response: res.weather }))
